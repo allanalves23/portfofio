faa91ad01019f96441f7e65e08537b2167ac176f
Allan Wanderley Alves <awallan259@gmail.com>
2019-07-16 23:25:29 -0300

    add doc test eti

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')()
 express.route('/')
     .get((req, res) => res.sendFile('./index.html', {root: __dirname}))
 
+express.route('/api/estudante-ti')
+    .get((req, res) => res.sendFile('./docs/estudante-ti/index.html', {root: __dirname}))
+
 express.route('/test')
     .get((req, res) => res.status(200).send('Recurso test'))
 
