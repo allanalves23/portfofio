5f75a84be2117993578879587419021fccc0710c
Allan Wanderley Alves <awallan259@gmail.com>
2019-07-17 18:56:30 -0300

    Versão 1 lançada

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ app.use('/public', express.static('public'))
 app.route('/')
     .get((req, res) => res.sendFile('./index.html', {root: __dirname}))
 
+app.route('/contato')
+    .get((req, res) => res.sendFile('./contact.html', {root: __dirname}))
+
+app.route('/projetos')
+    .get((req, res) => res.sendFile('./projects.html', {root: __dirname}))
+
+app.route('/projetos/aps-poo')
+    .get((req, res) => res.sendFile('./projects/aps-poo.html', {root: __dirname}))
+
+app.route('/projetos/code-char')
+    .get((req, res) => res.sendFile('./projects/code-char.html', {root: __dirname}))
+
+
+app.get('/curriculum', (req, res) => res.download('./public/pdf/Allan_Curriculo.pdf'))
+
 app.route('/api/estudante-ti')
     .get((req, res) => res.sendFile('./docs/estudante-ti/index.html', {root: __dirname}))
 
-app.route('/test')
-    .get((req, res) => res.status(200).send('Recurso test'))
+
 
 app.listen(3000, () => console.log('executando na porta 3000'))
