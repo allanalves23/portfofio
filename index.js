57316083b4bb13666ccdfd43f379fb7c75664ad0
Allan Wanderley Alves <awallan259@gmail.com>
2019-08-11 21:09:32 -0300

    Change name projects

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.use('/public', express.static('public'))
 
 app.route('/')
     .get((req, res) => {
-        console.log(req.connection.remoteAddress)
         return res.sendFile('./index.html', {root: __dirname})
     })
 
@@ -19,8 +18,8 @@ app.route('/projetos')
 app.route('/projetos/aps-poo')
     .get((req, res) => res.sendFile('./projects/aps-poo.html', {root: __dirname}))
 
-app.route('/projetos/code-char')
-    .get((req, res) => res.sendFile('./projects/code-char.html', {root: __dirname}))
+app.route('/projetos/coder-mind')
+    .get((req, res) => res.sendFile('./projects/coder-mind.html', {root: __dirname}))
 
 
 app.get('/curriculum', (req, res) => res.download('./public/pdf/Allan_Curriculo.pdf'))
